a550089387b129b426f724368fa9ca272d70ea7d
waldamy <vladimir.dukhonchenko@gmail.com>
2018-12-03 20:23:03 +0200

    Tools.Mod() added + Tools.QuickSquareRoot() finished

--- AsymmetricCryptography/LabWorks/Lab3.cs
+++ AsymmetricCryptography/LabWorks/Lab3.cs
@@ -12,15 +12,7 @@ namespace AsymmetricCryptography.LabWorks
         private static Stopwatch stopwatch = new Stopwatch();
         static void Main()
         {
-            var ticks = (uint)(int)DateTime.Now.Ticks;
-            var generatorA = new LehmerHigh(ticks);
-
-            for (int i = 0; i <= 8 * 3; i++)
-            {
-                var a = generatorA.RandomBits(i);
-                if (i < 10) System.Console.WriteLine($"{i}  | {Tools.ToString(a)}");
-                else System.Console.WriteLine($"{i} | {Tools.ToString(a)}");
-            }
+            
         }
     }
 }
